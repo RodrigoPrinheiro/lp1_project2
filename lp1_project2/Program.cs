b88a356a47a5fe507ee882967626d6cfe156c4aa
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-05-18 21:12:40 +0100

    Add project files.

---
+++ lp1_project2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lp1_project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
