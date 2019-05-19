769efc8da89cbaca3a2a02149702764cb5aa322d
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-05-19 22:06:50 +0100

    Add position + Agent class and extensions

---
+++ lp1_project2/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lp1_project2
+{
+    public class Human : Agent
+    {
+
+        public Human(byte tag, Position position, bool input) : 
+            base(tag, input)
+        {
+            this.position = position;
+            Faction = ConsoleColor.Cyan;
+        }
+
+        public override Position Move()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{Tag}";
+        }
+    }
+}
---
+++ lp1_project2/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lp1_project2
+{
+    /// <summary>
+    /// Structure for a basic x,y position
+    /// </summary>
+    public struct Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
