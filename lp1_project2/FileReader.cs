e31a029b05bbf10b05e7e9e77e4ae567c917737e
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-05-28 22:08:57 +0100

    Add comments to FileReader class

--- lp1_project2/FileReader.cs
+++ lp1_project2/FileReader.cs
@@ -11,15 +11,33 @@ namespace lp1_project2
     /// </summary>
     class FileReader
     {
+        // File path, doesn't need any more files
         private const string currentGame = @"CurrentGameSave.txt";
 
+        /// <summary>
+        /// Reads from the current game save file and returns the value of a
+        /// key value passed in it's parameters.
+        /// </summary>
+        /// <param name="key">
+        /// Character of the key in which the value you want to get from the
+        /// 'ini' file is tagged.
+        /// </param>
+        /// <returns>
+        /// Returns the value of the asked key tag.
+        /// </returns>
         public static int ReadFromFile(char key)
         {
+            // Variable to save the current line read from the file.
             string line;
+
+            // Creates a StreamReader for the file using the safe space with
+            // the keyword 'using'
             using (StreamReader file = new StreamReader(currentGame))
             {
+                // Searches every line in the file
                 while ((line = file.ReadLine()) != null)
                 {
+                    // If the line has the correct Key then return it's value
                     string keyValue;
                     if (line[0].Equals(key))
                     {
