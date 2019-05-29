263b2797ac515f56f1919f8fa8720242842b2594
Rafael Castro e Silva <castroesilva.rafael@gmail.com>
2019-05-29 05:21:56 +0100

    Added position simulations for the 4 aditional maps.

--- lp1_project2/Board.cs
+++ lp1_project2/Board.cs
@@ -6,22 +6,22 @@ namespace lp1_project2
     class Board
     {
 
-        public int RealWidth {get; private set;}
-        public int RealHeight {get; private set;}
+        public int Width {get; private set;}
+        public int Height {get; private set;}
 
         Tile[,] realBoard;
 
-        Position[][] humanSimPositions;
-        Position[][] zombieSimPositions;
+        Dictionary<Human, Position[]> humanSimPositions;
+        Dictionary<Zombie, Position[]> zombieSimPositions;
 
-        List<Human> humansList;
-        List<Zombie> zombiesList;
+        public List<Human> humansList;
+        public List<Zombie> zombiesList;
 
         public Board(int width, int height, int nZ, int nH, int controllableZ, int controllableH)
         {
 
-            RealWidth = width;
-            RealHeight = height;
+            Width = width;
+            Height = height;
 
             humansList = new List<Human>();
             zombiesList = new List<Zombie>();
@@ -40,7 +40,10 @@ namespace lp1_project2
 
             zombiesList = MakeAgentList(nZ, controllableZ, Faction.Zombie) as List<Zombie>;
 
-            humansList = MakeAgentList(nH, controllableH, Faction.Human) as List<Human>;            
+            humansList = MakeAgentList(nH, controllableH, Faction.Human) as List<Human>;      
+
+            UpdateSimPositionsDictionary(Faction.Human);
+            UpdateSimPositionsDictionary(Faction.Zombie);
 
 
         }
@@ -84,12 +87,60 @@ namespace lp1_project2
                 }
                 else continue;
 
-
-                    
                 ctrlCounter++;
                 counter ++;
 
             }
+
+
+        }
+
+        void UpdateSimPositionsDictionary(Faction faction)
+        {
+            Position[] simPos = new Position[5];
+
+            if(faction == Faction.Human)
+            {
+
+                foreach (Human h in humansList)
+                {
+                    // In Clockwise order, starting at the center and going right
+                    simPos[0] = h.position;
+                    simPos[1] = 
+                    new Position(h.position.X + Width, h.position.Y);
+                    simPos[2] = 
+                    new Position(h.position.X, h.position.Y - Height);
+                    simPos[3] = 
+                    new Position(h.position.X - Width, h.position.Y);
+                    simPos[4] = 
+                    new Position(h.position.X, h.position.Y + Height);
+
+                    humanSimPositions.Add(h, simPos);
+
+                }
+            }
+
+            else if(faction == Faction.Zombie)
+            {
+
+                foreach (Zombie z in zombiesList)
+                {
+                    // In Clockwise order, starting at the center and going right
+                    simPos[0] = z.position;
+                    simPos[1] = 
+                    new Position(z.position.X + Width, z.position.Y);
+                    simPos[2] = 
+                    new Position(z.position.X, z.position.Y - Height);
+                    simPos[3] = 
+                    new Position(z.position.X - Width, z.position.Y);
+                    simPos[4] = 
+                    new Position(z.position.X, z.position.Y + Height);
+
+                    zombieSimPositions.Add(z, simPos);
+
+                }
+            }
+
 
 
         }
